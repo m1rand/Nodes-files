c93a9600cf33d3fa1edf459602299155f87261bf
m1rand <153674121+m1rand@users.noreply.github.com>
2025-06-11 05:58:10 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,23 +7,21 @@ package_name = 'chair_detector'
 setup(
     name=package_name,
     version='0.0.1', 
-    packages=find_packages(exclude=['test']), # Найдет 'chair_detector' как Python-пакет (директорию с __init__.py)
+    packages=find_packages(exclude=['test']), 
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]), # 'resource/chair_detector'
-        ('share/' + package_name, ['package.xml']), # 'chair_detector/package.xml'
-        # Пути для launch и rviz файлов, если они на верхнем уровне пакета
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')), # Ищет в 'launch/*.py' относительно setup.py
-        (os.path.join('share', package_name, 'rviz'),   glob('rviz/*.rviz')),   # Ищет в 'rviz/*.rviz' относительно setup.py
-        # Если у вас есть директория config, добавьте и ее:
-        # (os.path.join('share', package_name, 'config'), glob('config/*')), 
+            ['resource/' + package_name]), 
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')), 
+        (os.path.join('share', package_name, 'rviz'),   glob('rviz/*.rviz')),  
+
     ],
     install_requires=['setuptools', 'pyserial'],
     zip_safe=True,
     maintainer='mirand',
-    maintainer_email='your_actual_email@example.com', # Замените
-    description='Package for chair detection, including ESP32 lidar processing.', # Обновите
-    license='Apache License 2.0', # Замените
+    maintainer_email='your_actual_email@example.com', 
+    description='Package for chair detection, including ESP32 lidar processing.', 
+    license='Apache License 2.0', 
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
